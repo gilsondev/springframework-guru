71481e0778c4aea4ed02ad2c6767bf5a8762fa12
Gilson Filho <me@gilsondev.in>
2019-08-11 16:28:33 -0300

    Dependency Injection with Spring Framework

--- di-demo/src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
+++ di-demo/src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -1,15 +1,21 @@
 package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private GreetingService greetingService;
 
+    // Since Spring 4.2, the injection by constructor is work without @Autowired, because the framework consider that
+    // automatically. So this is optional now.
+    @Autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    String sayHello() {
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
 }
