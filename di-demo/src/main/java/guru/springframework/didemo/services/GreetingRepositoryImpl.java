6ae4923c72148a990396051969968f40805f3cff
Gilson Filho <me@gilsondev.in>
2019-08-21 21:38:37 -0300

    Inject Dependency Assignment Review

---
+++ di-demo/src/main/java/guru/springframework/didemo/services/GreetingRepositoryImpl.java
@@ -0,0 +1,21 @@
+package guru.springframework.didemo.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingRepositoryImpl implements GreetingRepository {
+    @Override
+    public String getEnglishGreeting() {
+        return "Hello - Primary Greeting Service";
+    }
+
+    @Override
+    public String getGermanGreeting() {
+        return "German Thing";
+    }
+
+    @Override
+    public String getSpanishGreeting() {
+        return "Servicio de Saludo Primario";
+    }
+}
